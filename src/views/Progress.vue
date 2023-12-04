5becfe5a38db82b6a8a4e61a6251c0af8ace9e6d
viktor_tumanov <viktor.tumanov@waveaccess.ru>
2023-12-04 17:13:40 +0300

    progress circle

--- src/views/Progress.vue
+++ src/views/Progress.vue
@@ -4,19 +4,26 @@
   <div class="line--block">
     <ProgressBar maxWidth="400px" :percent="percentBar" color="success"/>
     <div class="line">
-      <Button label="ádd 10%" @click="addPercentBar"/>
+      <Button label="add 10%" @click="addPercentBar"/>
       <Button label="reduce 10%" @click="reducePercentBar"/>
     </div>
-    
   </div>
-  
+
+  <h2 class="heading-2">Progress circle</h2>
+  <ProgressCircle color="danger" :percent="percentCircle"/>
+  <div class="line">
+      <Button label="add 10%" @click="addPercentCircle"/>
+      <Button label="reduce 10%" @click="reducePercentCircle"/>
+    </div>
 </template>
 <script setup>
 import {ref} from 'vue'
-import ProgressBar from '@/components/Progress/ProgressBar.vue';
-import Button from '@/components/Button.vue';
+import ProgressBar from '@/components/Progress/ProgressBar.vue'
+import ProgressCircle from '@/components/Progress/ProgressCircle.vue'
+import Button from '@/components/Button.vue'
 
 const percentBar = ref(10)
+const percentCircle = ref(60)
 
 const addPercentBar = () => {
   percentBar.value += 10
@@ -31,6 +38,22 @@ const reducePercentBar = () => {
 
   if (percentBar.value < 0) {
     percentBar.value = 0
+  }
+}
+
+const addPercentCircle = () => {
+  percentCircle.value += 10
+
+  if (percentCircle.value > 100) {
+    percentCircle.value = 100
+  }
+}
+
+const reducePercentCircle = () => {
+  percentCircle.value -= 10
+
+  if (percentCircle.value < 0) {
+    percentCircle.value = 0
   }
 }
 </script>
